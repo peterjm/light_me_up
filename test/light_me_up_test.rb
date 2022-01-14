9ad62ae61a88fea9edf1e10460c3b3562f82225c
Peter McCracken <peter@petermccracken.com>
2022-01-14 14:49:02 -0500

    remove failing test

--- test/light_me_up_test.rb
+++ test/light_me_up_test.rb
@@ -6,8 +6,4 @@ class LightMeUpTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::LightMeUp::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
